8e830d5e1e501ab9335592c58f6be98f71436902
oncekiller <canard.antoine@gmail.com>
2024-06-19 21:12:16 +0200

    add vue-landing lib

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { FAQSection } from '@oncekiller/vue-landing'
+</script>
 
 <template>
   <header></header>
 
-  <RouterView />
+  <FAQSection />
 </template>
 
 <style scoped></style>
